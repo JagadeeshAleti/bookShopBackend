7cddafc0f3d3e7ad4de307f6f0432ef3ffd6b21e
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-07-27 09:22:40 +0530

    DataBase connection

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const express = require('express')
 const app = express()
 
+const dotenv = require('dotenv')
+dotenv.config()
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+db.on('error', error => console.log(error))
+db.once('open', () => console.log('Connected to the Databse'))
+
 app.listen(process.env.PORT || 9001)
