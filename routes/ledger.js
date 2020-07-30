b8d540a98773048cb73296a38928d3a352f44a7c
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-07-30 20:30:45 +0530

    Book with return status

--- routes/ledger.js
+++ routes/ledger.js
@@ -10,9 +10,27 @@ router.get('/:userId/information', async (req, res) => {
         const userInfo = await Ledger.find({"userId" : _id})
         res.send(userInfo)
     } catch(error) {
-        console.log(error)
         res.status(500).send(error)
     }
 })
 
+//CHECKOUT info
+router.get('/CHECKOUT', async (req, res) => {
+    try {
+        const checkoutInfo = await Ledger.find({"status": "CHECKOUT"})
+        res.send(checkoutInfo)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}) 
+
+//RETURN info
+router.get('/RETURN', async (req, res) => {
+    try {
+        const returnInfo = await Ledger.find({"status": "RETURN"})
+        res.send(returnInfo)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
 module.exports = router
