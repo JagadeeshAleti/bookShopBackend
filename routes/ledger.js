e4cfbe30c8492d97af5af097b984b3c2295b7451
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-08-02 10:13:06 +0530

    qeury params

--- routes/ledger.js
+++ routes/ledger.js
@@ -3,34 +3,15 @@ const Ledger = require('../models/ledger')
 
 const router = express.Router()
 
-//user information by id
-router.get('/:userId/information', async (req, res) => {
-    const _id = req.params.userId
-    try {
-        const userInfo = await Ledger.find({"userId" : _id})
-        res.send(userInfo)
-    } catch(error) {
-        res.status(500).send(error)
-    }
-})
 
-//CHECKOUT info
-router.get('/CHECKOUT', async (req, res) => {
-    try {
-        const checkoutInfo = await Ledger.find({"status": "CHECKOUT"})
+//Status info
+router.get('/filter', async (req, res) => {
+    try {    
+        const checkoutInfo = await Ledger.find(req.query)
         res.send(checkoutInfo)
     } catch (error) {
         res.status(500).send(error)
     }
 }) 
 
-//RETURN info
-router.get('/RETURN', async (req, res) => {
-    try {
-        const returnInfo = await Ledger.find({"status": "RETURN"})
-        res.send(returnInfo)
-    } catch (error) {
-        res.status(500).send(error)
-    }
-})
 module.exports = router
