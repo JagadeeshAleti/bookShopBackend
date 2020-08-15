184a359d49169759e776c5e518a44d1eda9fa639
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-08-15 10:48:55 +0530

    Pagination(skip, limit), logging

---
+++ config/logger.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const { 
+    createLogger,
+    transports,
+    format
+} = require('winston')
+require('winston-mongodb')
+
+const logger = createLogger({
+    transports: [
+        new transports.File({
+            filename: 'info.log',
+            level: 'info',
+            format: format.combine(format.timestamp(), format.json())
+        })        
+    ]
+})
+
+module.exports = logger
--- routes/ledger.js
+++ routes/ledger.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const Ledger = require('../models/ledger')
+const logger = require('../config/logger')
 
 const router = express.Router()
 
 
 //Status info
 router.get('/filter', async (req, res) => {
-    try {    
+    try { 
+        logger.info("Filter request made")  
         const checkoutInfo = await Ledger.find(req.query)
         res.send(checkoutInfo)
+        console.log("Filter request resolved")
     } catch (error) {
+        logger.error("Filter request failed")
         res.status(500).send(error)
     }
 }) 
