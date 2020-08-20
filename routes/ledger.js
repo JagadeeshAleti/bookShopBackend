d5c043f62b6ac07d14d32302bedf01ad22c7ad6c
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-08-20 20:31:27 +0530

    Pagination with filtering

--- routes/ledger.js
+++ routes/ledger.js
@@ -4,9 +4,24 @@ const logger = require('../config/logger')
 
 //Status info
 router.get('/filter', async (req, res) => {
+    const query = req.query
+    const status = query.status
+    const page = parseInt(query.page || '1')
+    const limit = parseInt(query.limit || '3')
+
+    const searchKeys = ['status', 'userId', 'bookId']
+    const queryObj = {}
+
+    searchKeys.forEach(x => {
+        if(query[x]) {
+            queryObj[x] = query[x]
+        }
+    })
+
+    console.log(queryObj)
     try { 
         logger.info("Filter request made")  
-        const checkoutInfo = await Ledger.find(req.query)
+        const checkoutInfo = await Ledger.find(queryObj).limit(limit).skip((page-1)*limit)
         res.send(checkoutInfo)
     } catch (error) {
         logger.error("Filter request failed")
